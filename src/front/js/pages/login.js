12f0c02e0afbd430d3cdcd3d1b0520f3c1365709
Tania Madrigal <129007972+TaniaMJ10@users.noreply.github.com>
2023-09-07 23:44:32 +0000

    formulario login

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+
+export const Login = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		
+        <div className="container mt-5">
+            <div className="card ">
+  <div className="card-header">
+    Login
+  </div>
+  <div className="card-body">
+  <form>
+  <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" placeholder= "email"aria-describedby="emailHelp"/>
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label for="exampleInputPassword1" className="form-label">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1"/>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+  </div>
+  <div className="card-footer text-muted">
+   
+  </div>
+</div>
+		</div>
+	);
+};
