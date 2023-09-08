9b012c23ec3f509fa6fd80933b4a37fcd4adb23d
Ashley Molina <131078848+Ashley8-prog@users.noreply.github.com>
2023-09-08 00:56:01 +0000

    RegisterLISTO

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -7,7 +7,7 @@ import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
-import Register from "./pages/register.jsx";
+import Register from "./pages/registro.jsx";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
@@ -34,6 +34,7 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<Register />} path="/register" />
+
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
