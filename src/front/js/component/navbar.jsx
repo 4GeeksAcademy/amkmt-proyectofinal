b36c14ec403649e96dbcce688e9c8ac70275f6ab
Kianny G. Marín <132239147+kiannygmarin@users.noreply.github.com>
2023-09-12 00:50:40 +0000

    Agregué el Navbar

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 
-const Navbar = () => {
+const Navbar2 = () => {
     return (
         <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">StartBootstrap</a>
+                <a className="navbar-brand text-warning" href="#">La casita feliz</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse d-line justify-content-end" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                            <a className="nav-link active text-warning" aria-current="page" href="#">Inicio</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Menú</a>
+                            <a className="nav-link text-warning" href="#">Menú</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Locales</a>
+                            <a className="nav-link text-warning" href="#">Locales</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link "href="#">Ingresar</a>
+                            <a className="nav-link text-warning" href="#">Ingresar</a>
                         </li>
                     </ul>
                 </div>
@@ -29,4 +29,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar
+export default Navbar2
