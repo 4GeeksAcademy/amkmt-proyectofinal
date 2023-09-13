cdcbd026e142c70f6485e1b05e78e076e51891bf
Kianny G. Marín <132239147+kiannygmarin@users.noreply.github.com>
2023-09-13 00:39:03 +0000

    Cambios

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,54 +2,56 @@ import React from "react";
 
 import "../../styles/index.css";
 
-
-import Jumbotron from "../component/jumbotron.jsx"
-
-import Card from "../component/card.jsx"
+import { Link } from 'react-router-dom';
 
 
 //create your first component
 const Home = () => {
 	return (
 		<>
-		<div className="container py-3">
-			<Jumbotron/>
-			<div className="row ms-2 my-3 d-inline-flex justify-content-center">
-			<div className="col-lg-3 col-sm-12 mx-3">
-			<Card/>
+			<div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
+				<div className="carousel-indicators">
+					<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+					<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+					<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+				</div>
+				<div className="carousel-inner">
+					<div className="carousel-item active" data-bs-interval="10000">
+						<img src="https://fondosmil.com/fondo/73620.jpg" className="d-block w-100" alt="..." />
+						<div className="carousel-caption d-none d-md-block">
+							<h5>Revisa nuestro menú</h5>
+							<p>Bienvenidos a La Casita Feliz</p>
+							<Link className="nav-link text-dark" to="/menu">Menú</Link>
+						</div>
+					</div>
+					<div className="carousel-item" data-bs-interval="2000">
+						<img src="https://fondosmil.com/fondo/73680.jpg" className="d-block w-100" alt="..." />
+						<div className="carousel-caption d-none d-md-block">
+							<h5>Revisa nuestro menú</h5>
+							<p>Bienvenidos a La Casita Feliz</p>
+							<Link className="nav-link text-dark" to="/menu">Menú</Link>
+						</div>
+					</div>
+					<div className="carousel-item">
+						<img src="https://fondosmil.com/fondo/73680.jpg" className="d-block w-100" alt="..." />
+						<div className="carousel-caption d-none d-md-block">
+							<h5>Revisa nuestro menú</h5>
+							<p>Bienvenidos a La Casita Feliz</p>
+							<Link className="nav-link text-dark" to="/menu">Menú</Link>
+						</div>
+					</div>
+				</div>
+				<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+					<span className="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span className="visually-hidden">Previous</span>
+				</button>
+				<button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+					<span className="carousel-control-next-icon" aria-hidden="true"></span>
+					<span className="visually-hidden">Next</span>
+				</button>
 			</div>
-			<div className="col-lg-3 col-sm-12 mx-3">
-			<Card/>
-			</div>
-			<div className="col-lg-3 col-sm-12 mx-3">
-			<Card/>
-			</div>
-			<div className="col-lg-3 col-sm-12 mx-3">
-			<Card/>
-			</div>
-			<div className="col-lg-3 col-sm-12 mx-3">
-			<Card/>
-			</div>
-			<div className="col-lg-3 col-sm-12 mx-3">
-			<Card/>
-			</div>
-			<div className="col-lg-3 col-sm-12 mx-3">
-			<Card/>
-			</div>
-			<div className="col-lg-3 col-sm-12 mx-3">
-			<Card/>
-			</div>
-			<div className="col-lg-3 col-sm-12 mx-3">
-			<Card/>
-			</div>
-		</div>
-		</div>
-		
-		<div>
-
-		</div>
 		</>
-		
+
 	);
 };
 
