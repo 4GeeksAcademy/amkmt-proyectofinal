406de77756e86ac3732b156558e1276607bf5d40
luciasaxjix2 <128443618+luciasaxjix2@users.noreply.github.com>
2023-09-14 14:38:55 +0000

    Login listo

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,11 +5,18 @@ from flask import Flask, request, jsonify, url_for, Blueprint,  current_app
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from base64 import b64encode
+import os 
 api = Blueprint('api', __name__)
+
+def set_password(password, salt):
+    return generate_password_hash(f"{password}{salt}")
+
 
 def check_password(hash_password, password, salt):
     return check_password_hash(hash_password, f"{password}{salt}")
@@ -34,11 +41,14 @@ def signup():
     age = request.json.get("age", None)
     city = request.json.get("city", None)
     phone = request.json.get("phone", None)
+
+    salt = b64encode(os.urandom(32)).decode('utf-8')
+    password = set_password(password, salt) 
     existing_email = User.query.filter_by(email=email).first()
     if existing_email:
         return jsonify({"msg": "email already exists"}), 400
     new_user = User(username=username, password=password, address=address,
-                    name=name, age=age, city=city, phone=phone, email=email, is_active=True)
+                    name=name, age=age, city=city, phone=phone, email=email, salt=salt )
     db.session.add(new_user)
     db.session.commit()
     return jsonify({"user_id": new_user.id}), 200
@@ -51,18 +61,24 @@ def login():
         email = body.get("email", None)
         password = body.get("password", None)
 
-        if email is "test" or password is "test":
+        if email is None or password is None :
             return jsonify("You need an email and a password"), 400
         else:
             user = User.query.filter_by(email=email).one_or_none()
             if user is None:
                 return jsonify({"message": "Bad credentials"}), 400
             else:
-                if check_password(user.password, password, user.salt):
+                if check_password(user.password,password,user.salt):
                     token = create_access_token(identity=user.id)
                     return jsonify({"token": token}), 200
                 else:
                     return jsonify({"message": "Bad credentials"}), 400
+                
+            #    if check_password(user.password, password, user.salt):
+            #         token = create_access_token(identity=user.id)
+            #         return jsonify({"token": token}), 200
+            #     else:
+            #         return jsonify({"message": "Bad credentials"}), 400
     
     access_token = create_access_token(identity =email)
     return jsonify(access_token= access_token)
