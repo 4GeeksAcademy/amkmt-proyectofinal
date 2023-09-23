d747490f4b9bd3251860352273a95835b3ccf309
luciasaxjix2 <128443618+luciasaxjix2@users.noreply.github.com>
2023-09-23 14:40:23 +0000

    Pruebas, sobre nosotros

---
+++ src/front/js/pages/contacto.js
@@ -0,0 +1,35 @@
+<section class="contacto">
+    <div class="info-contacto">
+        <div class="direccion">
+            <h3>Dirección</h3>
+            <p>Calle Principal 123</p>
+            <p>Ciudad, Estado</p>
+        </div>
+        
+        <div class="telefono">
+            <h3>Teléfono</h3>
+            <p>Tel: (123) 456-7890</p>
+        </div>
+        
+        <div class="correo">
+            <h3>Correo Electrónico</h3>
+            <p>Email: info@turestaurante.com</p>
+        </div>
+    </div>
+
+    <div class="formulario"/>
+        <h2>Envíanos un Mensaje</h2>
+        <form id="contactForm">
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre" required>
+
+            <label for="email">Correo Electrónico:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="mensaje">Mensaje:</label>
+            <textarea id="mensaje" name="mensaje" rows="4" required></textarea>
+
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
+</section>
