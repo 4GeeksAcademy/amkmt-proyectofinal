96367ef1c6c20bc0a47e985dd903bba41f385510
luciasaxjix2 <128443618+luciasaxjix2@users.noreply.github.com>
2023-09-27 18:52:55 +0000

    Vista Admi, crear productos

--- src/front/js/component/carousel.jsx
+++ src/front/js/component/carousel.jsx
@@ -5,6 +5,7 @@ import Foto2 from '../../img/GeeksCoffee2.png'
 import Foto3 from '../../img/GeeksCoffee3.png'
 import Recetas1 from "../../img/Recetas1.png"
 import Recetas2 from "../../img/Recetas2.png"
+// import Historia from "../../img/Historia.png"
 
 
 
@@ -26,6 +27,12 @@ const Carousel = () => {
             <img src={Recetas1} className="d-block w-100" alt="" />
             <img src={Recetas2} className="d-block w-100" alt="" />
             </div>
+
+            {/* <div className="Sobre Nosotros">
+            <img src={Historia} className="d-block w-100" alt="" />
+            </div> */}
+
+
         </div>
 
 
--- src/front/js/pages/crearMenu.js
+++ src/front/js/pages/crearMenu.js
@@ -3,19 +3,50 @@ import { Context } from "../store/appContext";
 import Swal from "sweetalert2";
 import "../../styles/prueba.css";
 
+const estadoInicial = {
+
+    name: "",
+    description: "",
+    image: "",
+    price: "",
+}
+
 const CrearMenu = () => {
-    const [name, setName] = useState("");
-    const [description, setDescription] = useState("");
-    const [image, setImage] = useState("");
-    const [price, setPrice] = useState("");
     const { store, actions } = useContext(Context);
-    const agregarMenu = (event) => {
-        event.preventDefault();
-        actions.agregarMenu(name, description, image, price)
-        setName("");
-        setDescription("");
-        setImage("");
-        setPrice("");
+    const [producto, setProducto] = useState(estadoInicial);
+
+    const handleChanges = (event) => {
+        setProducto({
+            ...producto,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const agregarMenu = async () => {
+
+        const formData = new FormData()
+        formData.append("name", producto.name)
+        formData.append("description", producto.description)
+        formData.append("image", producto.image)
+        formData.append("price", producto.price)
+
+        let response = await actions.agregarMenu(formData)
+        if (response == 201) {
+            setProducto(estadoInicial)
+            Swal.fire(
+                "Producto Creado Exitosamente",
+                "Congratulation",
+                "success"
+            )
+
+        }
+
+        // event.preventDefault();
+        // actions.agregarMenu(name, description, image, price)
+        // setName("");
+        // setDescription("");
+        // setImage("");
+        // setPrice("");
     }
 
     return (
@@ -27,32 +58,44 @@ const CrearMenu = () => {
                         <div className="    ">
                             <div className="input-field" id="nameInput">
                                 <i class="fa-solid fa-utensils"></i>
-                                <input type="text" placeholder="Nombre" onChange={
-                                    (event) => setName(event.target.value)
-                                }
-                                    value={name} />
+                                <input
+                                    type="text"
+                                    placeholder="Nombre"
+                                    name="name"
+                                    onChange={handleChanges}
+                                    value={producto.name} />
                             </div>
                             <div className="input-field">
                                 <i class="fa-regular fa-file-lines"></i>
-                                <input type="description" placeholder="descripcion" onChange={
-                                    (event) => setDescription(event.target.value)
-                                }
-                                    value={description} />
+                                <input
+                                    type="description"
+                                    placeholder="descripcion"
+                                    name="description"
+                                    onChange={handleChanges}
+                                    value={producto.description} />
                             </div>
+
                             <div className="input-field">
                                 <i class="fa-regular fa-image"></i>
-                                <input type="text" placeholder="imagen" onChange={
-                                    (event) => setImage(event.target.value)
-                                }
-                                    value={image} />
+                                <input
+                                    type="file"
+                                    placeholder="imagen"
+                                    name="image"
+                                    onChange={(event) => setProducto({ ...producto, image: event.target.files[0] })}
+
+
+                                />
                             </div>
 
                             <div className="input-field" id="nameInput">
                                 <i class="fa-solid fa-tag"></i>
-                                <input type="text" placeholder="precio" onChange={
-                                    (event) => setPrice(event.target.value)
-                                }
-                                    value={price} />
+                                <input
+                                    type="text"
+                                    name="price"
+                                    placeholder="precio"
+                                    onChange={handleChanges}
+
+                                    value={producto.price} />
                             </div>
                         </div>
                         <div className="btn-field">
