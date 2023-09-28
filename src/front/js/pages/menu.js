9dcdf25c4443028591b17e5582891a6b44012966
Tania Madrigal <129007972+TaniaMJ10@users.noreply.github.com>
2023-09-28 03:40:47 +0000

    cambios varios

--- src/front/js/pages/menu.js
+++ src/front/js/pages/menu.js
@@ -1,49 +1,55 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
 
 import "../../styles/index.css";
 import "../../styles/carousel.css";
 
-
 import Jumbotron from "../component/jumbotron.jsx";
-import { Link } from "react-router-dom";
 import Card from "../component/card.jsx";
-
-
+import { Link } from "react-router-dom";
 
 //create your first component
 const Menu = () => {
+	const { store, actions } = useContext(Context)
 	return (
 		<>
 			<div className="container py-3">
 				<Jumbotron />
 				<div className="row ms-2 my-3 d-inline-flex justify-content-center">
-					<div className="col-lg-3 col-sm-12 mx-3">
-						<Card />
-					</div>
-					<div className="col-lg-3 col-sm-12 mx-3">
-						<Card />
-					</div>
-					<div className="col-lg-3 col-sm-12 mx-3">
-						<Card />
-					</div>
-					<div className="col-lg-3 col-sm-12 mx-3">
-						<Card />
-					</div>
-					<div className="col-lg-3 col-sm-12 mx-3">
-						<Card />
-					</div>
-					<div className="col-lg-3 col-sm-12 mx-3">
-						<Card />
-					</div>
-					<div className="col-lg-3 col-sm-12 mx-3">
-						<Card />
-					</div>
-					<div className="col-lg-3 col-sm-12 mx-3">
-						<Card />
-					</div>
-					<div className="col-lg-3 col-sm-12 mx-3">
-						<Card />
-					</div>
+					{/* <div className="col-lg-3 col-sm-12 mx-3">
+                        <Card />
+                    </div>
+                    <div className="col-lg-3 col-sm-12 mx-3">
+                        <Card />
+                    </div>
+                    <div className="col-lg-3 col-sm-12 mx-3">
+                        <Card />
+                    </div>
+                    <div className="col-lg-3 col-sm-12 mx-3">
+                        <Card />
+                    </div>
+                    <div className="col-lg-3 col-sm-12 mx-3">
+                        <Card />
+                    </div>
+                    <div className="col-lg-3 col-sm-12 mx-3">
+                        <Card />
+                    </div>
+                    <div className="col-lg-3 col-sm-12 mx-3">
+                        <Card />
+                    </div>
+                    <div className="col-lg-3 col-sm-12 mx-3">
+                        <Card />
+                    </div>
+                    <div className="col-lg-3 col-sm-12 mx-3">
+                        <Card />
+                    </div> */}
+					{
+						store.products.map((item) => {
+							return (
+								<Card key={item.id} />
+							)
+						})
+					}
 				</div>
 			</div>
 
