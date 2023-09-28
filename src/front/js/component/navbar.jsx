d0dfbc2e50faadacec1ade765ce59bc7f6ee0989
luciasaxjix2 <128443618+luciasaxjix2@users.noreply.github.com>
2023-09-28 07:16:14 +0000

    Resolviendo

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -39,11 +39,11 @@ const Navbar2 = () => {
                             <li className="nav-item">
                                 <Link className="nav-link  colorenlace " to="/register">Registrarse</Link>
                             </li>
-                            <div class="dropdown dropdowncerrar">
+                            <div className="dropdown dropdowncerrar">
                                 <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
 
                                 </button>
-                                <ul class="dropdown-menu">
+                                <ul className="dropdown-menu">
                                     <li className="nav-item botonsalir">
                                         <Link className="nav-link colorenlace" to="/logout">Salir</Link>
                                     </li>
--- src/front/js/pages/administrador.jsx
+++ src/front/js/pages/administrador.jsx
@@ -1,34 +1,27 @@
-import React from "react"
-import {Link} from "react-router-dom"
-import Button from 'react-bootstrap/Button';
-import "../../styles/administrador.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import "../../styles/administrador.css";
 
-
-  
-
-const Administrador = () =>{
- 
+const Administrador = () => {
     return (
         <div className="container">
             <h2>Administrador </h2>
-            <div>
-            <div className="Button">
-                    <Link to = "/productos">
-                        <Button variant="primary" size="lg">
-                         Productos
-                         </Button>
-                    </Link>
+            <div className="container-Button">
+                <Link to="/productos">
                     <Button variant="primary" size="lg">
-                         Reservas
-                         </Button>
-                    <Button variant="primary" size="lg">
-                         Usuarios
-                     </Button>
-                </div>
+                        Productos
+                    </Button>
+                </Link>
+                <Button variant="primary" size="lg">
+                    Reservas
+                </Button>
+                <Button variant="primary" size="lg">
+                    Usuarios
+                </Button>
             </div>
-                
         </div>
-    )
-}
+    );
+};
 
-export default Administrador 
+export default Administrador;
