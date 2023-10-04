fc9b89bdaf1dd61f61fb8f7317d6e11a92a77039
Tania Madrigal <129007972+TaniaMJ10@users.noreply.github.com>
2023-10-04 18:54:13 +0000

    avance 2 reservas

--- src/front/js/pages/reservasadmin.jsx
+++ src/front/js/pages/reservasadmin.jsx
@@ -1,18 +1,33 @@
 import React, { useEffect,useContext } from 'react';
-import { useStore } from 'tu-libreria-de-estado'; // Asegúrate de importar la librería que estás utilizando
+
 
 import { Context } from "../store/appContext";
 const ReservasAdmin = () => {
     const { store, actions } = useContext(Context)
-    console.log(store.reservationData)
-
+    console.log(store.current_user)
+  //  console.log(store.current_user.name)
+    //console.log(store.current_user.reservas)
     useEffect(() => {
-        // Puedes realizar alguna lógica adicional aquí, si es necesario, al cargar la vista
+       actions.getAuth()
     }, []);
 
     return (
         <div>
             <h2>Reservas</h2>
+            {/* <div className="list-group">
+                    {store.current_user.map((item) => (
+                        <label className="list-group-item row" key={item.id} >
+                            <div className="row">
+                                <div className="col-lg-1">{item.name}</div>
+                                <div className="col-lg-5">{item.reservas.cantidad_personas}</div>
+                                
+                                <button className="col-lg-2 mb-3" type="button" onClick={() => borrarMenu(item.id)}>
+                                    <i className="fa-solid fa-trash"></i>
+                                </button>
+                            </div>
+                        </label>
+                    ))}
+                </div> */}
             
         </div>
     );
