e787f36f2e909a1b74bb0570976c7a90fa2fdbbe
Ashley Molina <131078848+Ashley8-prog@users.noreply.github.com>
2023-10-05 23:01:18 +0000

    guardandofinales

--- src/front/js/component/jumbotron.jsx
+++ src/front/js/component/jumbotron.jsx
@@ -4,11 +4,10 @@ import "../../styles/titulo.css";
 const Jumbotron = () => {
     return (
         <div className="bg-light">
-            <h1>Deleitar con platos creativos y deliciosos, inspirados en la cultura geek.
+            <h1 className='deleitar'>Deleitar con platos creativos y deliciosos, inspirados en la cultura geek.
             </h1>
-            <h5>Donde la programación se combina con el sabor</h5>
-            
-            </div>
+
+        </div>
     )
 }
 
